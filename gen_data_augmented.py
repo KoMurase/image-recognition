61e36a1ea82ac78c2814576febe938caa43792ab
Ko Murase <mkou0819@outlook.jp>
2019-04-11 17:00:34 +0900

    need review

---
+++ gen_data_augmented.py
@@ -0,0 +1,63 @@
+from PIL import Image
+import os,glob
+import numpy as np
+from sklearn import model_selection
+classes=["monkey","boar","crow"]
+num_classes=len(classes)
+image_size=50
+num_testdata=100
+
+#画像の読み込み
+X_train=[]
+X_test=[]
+Y_train=[]
+Y_test=[]
+
+for index, classlabel in enumerate(classes):
+    photos_dir="./"+ classlabel
+    files=glob.glob(photos_dir+"/*.jpg")
+    for i, file in enumerate(files):
+        if i>= 200: break
+        image=Image.open(file)
+        #open() は PIL.Image というモジュールのグローバル関数
+        image=image.convert("RGB")
+        image=image.resize((image_size,image_size))
+        #convert や resize は Image.open() が返す画像オブジェクトが
+        #持つメソッドなので、image.convert(), image.resize()
+        #と呼び出します
+        data=np.asarray(image)
+
+        if i<num_testdata:
+            X_test.append(data)
+            Y_test.append(index)
+        else:
+            X_train.append(data)
+            Y_train.append(index)
+
+            #データ量を増やすため回転と反転をする
+            for angle in range(-20,20,5):
+                #回転
+                img_r=image.rotate(angle)
+                data=np.asarray(img_r)
+                X_train.append(data)
+                Y_train.append(index)
+
+                #反転
+                img_trans=img_r.transpose(Image.FLIP_LEFT_RIGHT)
+                data=np.asarray(img_trans)
+                X_train.append(data)
+                Y_train.append(index)
+
+#X=np.array(X)
+#Y=np.array(Y)
+X_train=np.array(X_train)
+X_test=np.array(X_test)
+y_train=np.array(Y_train)
+y_test=np.array(Y_test)
+
+
+#X_train,X_test,y_train,y_test=model_selection.train_test_split(X,Y)
+xy=(X_train,X_test,y_train,y_test)
+np.save("./animal_aug.npy",xy)
+
+#activate tf140で実行
